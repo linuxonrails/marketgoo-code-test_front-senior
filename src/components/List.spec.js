b228b75cec60911451babc2c75834048858a65fe
David Moreno <linuxonrails@linuxonrails.com>
2023-01-26 21:19:16 +0100

    First step with List.spec.js

--- src/components/List.spec.js
+++ src/components/List.spec.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import List from "./List";
 
 describe("List", () => {
-    it("should render a list", async () => {
+    it("should render table with a player list", () => {
         // Arranage
         render(<List />);
         // Act
+        screen.getByText("Player");
+        screen.getByText("Score");
+        screen.getByText("Actions");
+    });
+
+    it("should render form to add new players", () => {
+        // Arranage
+        render(<List />);
+        // Act
+        screen.getByText("Add new players");
+        screen.getByRole("button", { name: "Add" });
     });
 });
