d5068ce3fcb747975f210ab49a6aa455f0791872
David Moreno <linuxonrails@linuxonrails.com>
2023-01-29 19:13:19 +0100

    Refact/Fix: cypress tests stub the API response (not necesary the DB reset)

--- cypress/e2e/list.cy.js
+++ cypress/e2e/list.cy.js
@@ -1,49 +1,60 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+// This spec file contains a variety of tests for the List app.
+
+// The tests are written using the Cypress API.
+// https://on.cypress.io/api
 
 describe("List app", () => {
     beforeEach(() => {
-        // Reset DB:
-        // !TODO: refact index.js to export a function to reset the DB and call it here
-
-        // Cypress starts out with a blank slate for each test
-        // so we must tell it to visit our website with the `cy.visit()` command.
-        // Since we want to visit the same URL at the start of all our tests,
-        // we include it in our beforeEach function so that it runs before each test
+        // Intercept HTTP requests
+        cy.intercept("/players", {
+            status: "ok",
+            data: [
+                {
+                    id: 2,
+                    name: "Jhon",
+                    team: "Cobrakay",
+                    score: 1270,
+                    createdAt: "2023-01-29 17:02:43.831 +00:00",
+                    updatedAt: "2023-01-29 17:33:00.004 +00:00",
+                },
+                {
+                    id: 1,
+                    name: "Peter",
+                    team: "Pandas",
+                    score: 1129,
+                    createdAt: "2023-01-29 17:02:43.831 +00:00",
+                    updatedAt: "2023-01-29 17:31:50.004 +00:00",
+                },
+            ],
+        }).as("getPlayers");
         cy.visit("http://localhost:3000");
     });
 
-    xit("displays three player by default", () => {
-        cy.get("[data-testid=player]").should("have.length", 3);
+    it("displays two players with player names when the API response has two players", () => {
+        cy.wait("@getPlayers");
+        cy.get("[data-testid=player]").should("have.length", 2);
 
-        // TODO: get all and check if they are there
-        cy.get("[data-testid=players]")
-            .should("include.text", "Tommy")
-            .should("include.text", "Jhon")
-            .should("include.text", "Peter");
-        // cy.get(".todo-list li").last().should("have.text", "Pandas");
+        cy.get("[data-testid=player]").first().should("include.text", "Jhon");
+        cy.get("[data-testid=player]").last().should("include.text", "Peter");
     });
 
     it("can add new players", () => {
         const name = "Dutch";
         cy.intercept("GET", "/players").as("getPlayers");
 
+        cy.intercept("POST", "/players", {
+            status: "ok",
+            data: { name: name, team: null, score: null },
+        }).as("postPlayers");
+
         cy.get("input[name=name").type(`${name}{enter}`);
 
+        // Wait for the POST request (adding a new player)
+        cy.wait("@postPlayers");
+
+        // Wait for the GET request (fetching all players)
         cy.wait("@getPlayers");
-        cy.get("[data-testid=player]")
-            // .should("have.length", 4)
-            .last()
-            .should("include.text", name);
     });
 });
